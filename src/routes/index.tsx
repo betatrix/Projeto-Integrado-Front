6a3ddb5475a2aa340dcbd254777d735dee7b0d3d
Suanne Barbosa <suanne.a.barbosa@gmail.com>
2024-05-02 23:48:28 -0300

    feat(frontend): create first version of institution registration; include routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { Dashboard } from "../containers/Admin Dashboard/index";
+import {CadastroInstituicao} from '../containers/Institution Register/index';
+import { Course } from "../containers/Institution Register/searchCourse";
+
+
+
+export const AppRoutes = () =>{
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/cadastro" element={<CadastroInstituicao/>}/>
+                <Route path="/cursos" element={<Course/>}/>
+                <Route path="/pagina-inicial" element={<Dashboard/>}/>
+                <Route path="*" element={<Navigate to="/pagina-inicial" replace />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
