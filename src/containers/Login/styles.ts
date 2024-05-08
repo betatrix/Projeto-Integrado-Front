a1055b6054e6d2e3ea433a68061fda36eb941575
Isadora Vieira <isavieiracamara@gmail.com>
2024-05-07 23:08:36 -0300

    feat(institution): add image login

--- src/containers/Login/styles.ts
+++ src/containers/Login/styles.ts
@@ -1,10 +1,31 @@
-// estilização de componentes da página de dashboard do admin
 import styled from 'styled-components';
+
+export const LoginWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  /* box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); */
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 0;
+  margin-top: 40px;
+`;
 
 export const LoginContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   gap: 20px;
-  margin-top: 50px;
+  /* box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  padding: 10px, 0px, 10px 0px; */
+  max-width: 700px;
+  max-height: 500px;
+  margin-top: 100px;
+  margin-right: 50px;
+`;
+
+export const Image = styled.img`
+  max-width: 650px;
+  padding: 0;
+  margin: 0;
 `;
