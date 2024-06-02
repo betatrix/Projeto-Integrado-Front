915a29ad9736c3d057512c0ce3c48aedb57d67dc
Isadora Vieira <isavieiracamara@gmail.com>
2024-06-01 23:59:42 -0300

    feat(vocacionalTest): add vocacional test screen

---
+++ src/containers/HomePage/index.tsx
@@ -0,0 +1,79 @@
+
+import { Grid, Box, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import InitialPageHeader from '../../components/HomeHeader/';
+import Footer from '../../components/AdminFooter';
+import { Subtitle } from '../HomePage/styles';
+import studentLeft from '../../assets/img/studentLeft.png';
+
+const HomePage = () => {
+    return (
+        <>
+            <InitialPageHeader />
+            <Box sx={{ height: 'calc(100vh - 64px)', padding: '20px' }}>
+                <Grid container spacing={4} alignItems="center" justifyContent="center" sx={{ height: '100%' }}>
+                    <Grid item xs={12} md={6} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', textAlign: 'center', height: '100%' }}>
+                        <Box sx={{ mb: '5px', mt: '100px', ml:'80px' }}>
+                            <Subtitle>
+                                Bem-vindo a VOCCO!
+                            </Subtitle>
+                            <Typography variant="h3" sx={{ mb: '20px', mt: '30px' }} >
+                                Encontre Universidades <br /> e faça Testes Vocacionais <br /> para decidir seu futuro <br /> profissional.
+                            </Typography>
+                        </Box>
+                        <Box sx={{ mt: '20px', ml:'60px' }}>
+                            <Grid container spacing={2} justifyContent="center">
+                                <Grid item>
+                                    <Link to="/teste-vocacional" style={{ textDecoration: 'none' }}>
+                                        <Button
+                                            variant="contained"
+                                            size="large"
+                                            sx={{
+                                                backgroundColor: '#5479f7',
+                                                borderRadius: '20px',
+                                                padding: '20px 40px',
+                                                fontSize: '18px',
+                                                fontWeight: 'bold',
+                                                '&:hover': {
+                                                    backgroundColor: '#415bc4'
+                                                }
+                                            }}
+                                        >
+                                            Teste Vocacional
+                                        </Button>
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link to="/instituicao" style={{ textDecoration: 'none' }}>
+                                        <Button
+                                            variant="contained"
+                                            size="large"
+                                            sx={{
+                                                backgroundColor: '#5479f7',
+                                                borderRadius: '20px',
+                                                padding: '20px 40px',
+                                                fontSize: '18px',
+                                                fontWeight: 'bold',
+                                                '&:hover': {
+                                                    backgroundColor: '#415bc4'
+                                                }
+                                            }}
+                                        >
+                                            Universidades
+                                        </Button>
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={6} sx={{ textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+                        <img src={studentLeft} alt="Imagem representativa" style={{ maxWidth: '80%', maxHeight: '100%' }} />
+                    </Grid>
+                </Grid>
+            </Box>
+            <Footer />
+        </>
+    );
+};
+
+export default HomePage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import  Dashboard  from '../containers/AdminDashboard/index';
+import Dashboard from '../containers/AdminDashboard/index';
 import {CadastroInstituicao} from '../containers/Institution Register/index';
 import { BuscaCurso } from '../containers/Institution Register/searchCourse';
 import InstitutionManagement from '../containers/Institution Management/index';
@@ -9,6 +9,12 @@ import NovaSenha from '../containers/New Password';
 import RecuperarSenha from '../containers/Recover Password';
 import SucessPassword from '../containers/SucessPassword';
 import { InstitutionProvider } from '../context/institutionContext';
+// import HomePage from '../containers/HomePage';
+// import StudentDashboard from '../containers/StudentDashboard';
+// import InstitutionList from '../containers/HomePage/searchInstitution';
+// import About from '../containers/HomePage/about';
+// import CourseList from '../containers/Course Management';
+import VocacionalTest from '../containers/Vocacional Test';
 
 export const AppRoutes = () =>{
     return(
@@ -23,6 +29,12 @@ export const AppRoutes = () =>{
                     <Route path="/nova-senha" element={<NovaSenha/>}/>
                     <Route path="/success-change-password" element={<SucessPassword/>}/>
                     <Route path="/register" element={<Register/>}/>
+                    {/* <Route path='/gerenciamento-curso' element={<CourseList/>}/>
+                    <Route path='/instituicao' element={<InstitutionList/>}/>
+                    <Route path='/estudante' element={<StudentDashboard />} />
+                    <Route path='/sobre' element={<About />} /> */}
+                    <Route path='/admin' element={<Dashboard />} />
+                    <Route path='/teste-vocacional' element={<VocacionalTest/>}/>
                     <Route path="/pagina-inicial" element={<Dashboard/>}/>
                     <Route path="*" element={<Navigate to="/login" replace />} />
                 </Routes>
