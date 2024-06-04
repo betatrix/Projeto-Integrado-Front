37a437e66fe526477c3433316b65d94d77484d22
betatrix <b.dandrade@hotmail.com>
2024-06-03 23:37:21 -0300

    feat(translation): adjust config file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GlobalStyle from './styles/globalStyles';
 import ReactDOM from 'react-dom/client';
 import App from './app';
-import './i18n';
+import './lib/i18n';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
@@ -11,15 +11,21 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </React.StrictMode>,
 );
 
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import Login from './containers/Login'
-// import GlobalStyle from './styles/globalStyles'
+//TODO(beatriz.andrade) utilizar nas páginas que precisa de tradução
+//TODO(beatriz.andrade) verificar modo de armazenar estado do handleChangeLanguage
 
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <GlobalStyle />
-//     <Login />
-//   </React.StrictMode>,
-// )
+// const{
+//     t,
+//     i18n: {
+//         changeLanguage, language
+//     },
+// } = useTranslation();
+
+// const[currentLanguage, setCurrentLanguage] = useState(language);
+
+// const handleChangeLanguage = () => {
+//     const newLanguage = currentLanguage === 'en' ? 'pt' : 'en'
+//     changeLanguage(newLanguage);
+//     setCurrentLanguage(newLanguage);
+// }
 
