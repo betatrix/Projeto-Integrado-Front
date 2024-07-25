49d788793922be2eabc3aecf070f362cf0cd5e24
Beatriz <b.dandrade@hotmail.com>
2024-07-24 21:35:13 -0300

    style(folders): adjust folders name and structure

---
+++ src/containers/student/homePage/about.tsx
@@ -0,0 +1,46 @@
+import { Box, Grid, Typography } from '@mui/material';
+import React from 'react';
+import {
+    aboutBoxStyles,
+    gridItemImageStyles,
+    imageStyles,
+    gridItemTextStyles,
+    dividerBoxStyles,
+    typographyBodyStyles,
+    typographySmallBodyStyles,
+    typographyAboutTitleStyles,
+    gridAboutContainerStyles,
+} from './styles';
+import backgroundIcone from '../../../assets/img/backgroundIcone.png';
+
+export const About: React.FC = () => {
+    return (
+        <>
+            <Box sx={aboutBoxStyles}>
+                <Grid container spacing={4} alignItems="center" justifyContent="center" sx={gridAboutContainerStyles}>
+                    <Grid item xs={12} md={6} sx={gridItemImageStyles}>
+                        <img src={backgroundIcone} alt="Ícone de fundo" style={imageStyles} />
+                    </Grid>
+                    <Grid item xs={12} md={6} sx={gridItemTextStyles}>
+                        <Typography variant="h3" sx={typographyAboutTitleStyles}>
+                            Sobre nós
+                        </Typography>
+                        <Box sx={dividerBoxStyles} />
+                        <Typography variant="body1" sx={typographyBodyStyles}>
+                            Nós somos a <b>VOCCO</b>, uma plataforma destinada a orientar alunos do ensino médio da rede pública de educação no Brasil,
+                             auxiliando-os na escolha de suas futuras carreiras profissionais.
+                        </Typography>
+                        <Typography variant="h4" sx={{marginLeft:'40px'}}>
+                            Nome Vocco
+                        </Typography>
+                        <Typography variant="body1" sx={typographySmallBodyStyles}>
+                            Nosso nome deriva da palavra <b>vocação</b> em Latim.
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
+        </>
+    );
+};
+
+export default About;
---
+++ src/containers/student/studentDashboard/index.tsx
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from 'react';
+import { Grid, Box, Typography, Card, CardContent, Paper, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import StudentHeader from '../../../components/studentHeader';
+import AnnouncementBar from './announcement';
+import { SquareButton, TextButton, CardContentBox, TestButton, homePageBoxStyles } from './styles';
+// import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import InsightsIcon from '@mui/icons-material/Insights';
+import StudentFooter from '../../../components/studentFooter';
+import { contarTeste, buscarTestesDeEstudante } from '../../../services/apiService'; // Importar as funções necessárias
+
+const StudentDashboard: React.FC = () => {
+    const [showAnnouncement, setShowAnnouncement] = useState(true);
+    const [testCount, setTestCount] = useState(0);
+    const [testHistory, setTestHistory] = useState<{ date: string, result: string }[]>([]); // State para histórico de testes
+
+    useEffect(() => {
+        // Função para buscar a contagem de testes
+        const fetchTestCount = async () => {
+            try {
+                const count = await contarTeste();
+                setTestCount(count);
+            } catch (error) {
+                console.error('Erro ao buscar contagem de testes:', error);
+            }
+        };
+
+        // Função para buscar o histórico de testes do estudante
+        const fetchTestHistory = async () => {
+            try {
+                const estudanteId = 1; // Substitua pelo ID real do estudante
+                const tests = await buscarTestesDeEstudante(estudanteId);
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                const formattedTests = tests.map((test: any) => ({
+                    date: test.data,
+                    result: test.teste, // Ajuste conforme necessário para obter parte do resultado
+                }));
+                setTestHistory(formattedTests);
+            } catch (error) {
+                console.error('Erro ao buscar histórico de testes:', error);
+            }
+        };
+
+        fetchTestCount(); // Chamar a função ao carregar o componente
+        fetchTestHistory(); // Chamar a função ao carregar o componente
+    }, []);
+
+    const handleCloseAnnouncement = () => {
+        setShowAnnouncement(false);
+    };
+
+    return (
+        <>
+            <StudentHeader />
+            {showAnnouncement && (
+                <AnnouncementBar
+                    imageUrl="URL_DA_IMAGEM"
+                    onClose={handleCloseAnnouncement}
+                />
+            )}
+            <Box sx={homePageBoxStyles}>
+                <Grid container justifyContent="center">
+                    <Grid container spacing={25} justifyContent="center">
+                        <Grid item>
+                            <Paper sx={{backgroundColor:'white'}}>
+                                <SquareButton>
+                                    <TextButton>Testes Preenchidos</TextButton>
+                                    <InsightsIcon style={{ fontSize: 80, color: 'linear-gradient(90deg, #040410, #302EB7)' }} />
+                                    <Typography variant="h3" component="div">
+                                        {testCount}
+                                    </Typography>
+                                </SquareButton>
+                            </Paper>
+                        </Grid>
+                        <Grid item>
+                            <Paper sx={{backgroundColor:'white'}}>
+                                <SquareButton>
+                                    <TextButton>Resultados</TextButton>
+                                    <BarChartIcon style={{ fontSize: 80, color: 'linear-gradient(90deg, #040410, #302EB7)' }} />
+                                    <CardContentBox>
+                                        <Card sx={{ width: '100%', mt: 2 }}>
+                                            <CardContent>
+                                                {testHistory.length > 0 ? (
+                                                    testHistory.map((test, index) => (
+                                                        <Typography key={index} variant="body2" color="text.secondary">
+                                                            {test.date}: {test.result.substring(0, 30)}...
+                                                        </Typography>
+                                                    ))
+                                                ) : (
+                                                    <Typography variant="body2" color="text.secondary">
+                                                        Nenhum teste realizado.
+                                                    </Typography>
+                                                )}
+                                            </CardContent>
+                                        </Card>
+                                    </CardContentBox>
+                                </SquareButton>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Box sx={{ mt: '20px', textAlign: 'center', marginTop: '50px' }}>
+                    <Link to="/teste-vocacional" style={{ textDecoration: 'none' }}>
+                        <Button variant="contained" size="large" sx={TestButton}>
+                            Fazer Teste Vocacional
+                        </Button>
+                    </Link>
+                </Box>
+                <StudentFooter />
+            </Box>
+        </>
+    );
+};
+
+export default StudentDashboard;
