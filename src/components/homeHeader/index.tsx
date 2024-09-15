30911231e1ed7e7279323371274bf90bbc17e9c7
Beatriz <b.dandrade@hotmail.com>
2024-09-15 19:09:11 -0300

    refact(home): add welcome page responsivity and sidebar

--- src/components/homeHeader/index.tsx
+++ src/components/homeHeader/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
     AppBar,
     Container,
@@ -5,64 +6,106 @@ import {
     Box,
     Typography,
     Button,
+    IconButton,
+    Drawer,
+    List,
+    ListItem,
 } from '@mui/material';
+import { 
+    Menu as MenuIcon, 
+    HouseOutlined, 
+    AssignmentIndOutlined, 
+    InfoOutlined, 
+    HelpOutline, 
+    Person2Outlined, 
+    Close as CloseIcon 
+} from '@mui/icons-material';
 import { Link as ScrollLink } from 'react-scroll';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { useMediaQuery } from '@mui/material';
 import LanguageMenu from '../translationButton';
-
-const styles = {
-    logo: {
-        mr: 2,
-        fontFamily: 'Poppins',
-        fontWeight: 700,
-        letterSpacing: '.3rem',
-        color: 'black',
-        textDecoration: 'none',
-    },
-    LoginText: {
-        mr: 2,
-        marginLeft: '10px',
-        fontFamily: 'Roboto, monospace',
-        fontSize: '15px',
-        backgroundColor: '#D9EEFF',
-        color: '#185D8E',
-        fontWeight: 700,
-        borderColor: '#185D8E',
-        borderRadius: '7px',
-        borderWidth: '2px',
-        boxShadow: '4px 4px 0px 1px rgba(0, 111, 255, 0.2)',
-        '&:hover': {
-            backgroundColor: '#C0E3FF',
-            borderColor: '#185D8E',
-            borderWidth: '2px',
-        },
-    },
-    menu: {
-        mt: '45px',
-    },
-    linkButton: {
-        fontFamily: 'Roboto, monospace',
-        fontSize: '15px',
-        color: '#232235',
-        fontWeight: 700,
-        textDecoration: 'none',
-        marginRight: '10px',
-        cursor: 'pointer',
-    },
-};
+import { linkButtonMobile, logo, linkButton, LoginText } from './styles';
 
 function InitialPageHeader() {
     const navigate = useNavigate();
-    const{ t } = useTranslation();
+    const { t } = useTranslation();
+    const isMobile = useMediaQuery('(max-width:600px)');
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
     const handleLogin = () => {
         navigate('/login');
     };
 
+    const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
+            return;
+        }
+        setDrawerOpen(open);
+    };
+
+    const handleCloseDrawer = () => {
+        setDrawerOpen(false);
+    };
+
+    const menuItems = (
+        <List>
+            {/* Ícone de Fechar no topo */}
+            <ListItem sx={{ justifyContent: 'flex-end' }}>
+                <IconButton onClick={handleCloseDrawer} sx={{ color: '#185D8E' }}>
+                    <CloseIcon />
+                </IconButton>
+            </ListItem>
+
+            <ListItem>
+                <Person2Outlined style={{ color: '#185D8E' }} />
+                <ScrollLink onClick={handleLogin} smooth={true} duration={500} onSetActive={handleCloseDrawer}>
+                    <Button color="inherit" sx={linkButtonMobile}>{t('login')}</Button>
+                </ScrollLink>
+            </ListItem>
+            <ListItem>
+                <HouseOutlined style={{ color: '#185D8E' }} />
+                <ScrollLink to="home" smooth={true} duration={500} onSetActive={handleCloseDrawer}>
+                    <Button color="inherit" sx={linkButtonMobile}>{t('home')}</Button>
+                </ScrollLink>
+            </ListItem>
+            <ListItem>
+                <AssignmentIndOutlined style={{ color: '#185D8E' }} />
+                <ScrollLink to="testInformation" smooth={true} duration={500} onSetActive={handleCloseDrawer}>
+                    <Button color="inherit" sx={linkButtonMobile}>{t('testInformation')}</Button>
+                </ScrollLink>
+            </ListItem>
+            <ListItem>
+                <InfoOutlined style={{ color: '#185D8E' }} />
+                <ScrollLink to="about" smooth={true} duration={500} onSetActive={handleCloseDrawer}>
+                    <Button color="inherit" sx={linkButtonMobile}>{t('about')}</Button>
+                </ScrollLink>
+            </ListItem>
+            <ListItem>
+                <HelpOutline style={{ color: '#185D8E' }} />
+                <ScrollLink to="faq" smooth={true} duration={500} onSetActive={handleCloseDrawer}>
+                    <Button color="inherit" sx={linkButtonMobile}>{t('faq')}</Button>
+                </ScrollLink>
+            </ListItem>
+
+            {/* Direitos Reservados no canto inferior direito */}
+            <Box
+                sx={{
+                    position: 'absolute',
+                    color: '#185D8E',
+                    fontFamily: 'Roboto, sans-serif',
+                    paddingTop: '30rem',
+                    fontSize: '10px',
+                }}
+            >
+                © 2024 Vocco. Todos os direitos reservados.   
+            </Box>
+        </List>
+    );
+
     return (
         <AppBar style={{ backgroundColor: '#F3F3F3', boxShadow: 'none' }}>
-            <Container maxWidth="xl">
+            <Container maxWidth={isMobile ? "md" : "xl"}>
                 <Toolbar sx={{ justifyContent: 'space-between', height: '100px' }}>
                     <Box sx={{ display: 'flex', alignItems: 'center' }}>
                         <ScrollLink to="home" smooth={true} duration={500}>
@@ -71,36 +114,63 @@ function InitialPageHeader() {
                                 noWrap
                                 component="a"
                                 href="/pagina-inicial"
-                                sx={styles.logo}
+                                sx={logo}
                             >
                                 VOCCO
                             </Typography>
                         </ScrollLink>
                     </Box>
-                    <Box sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                    }}>
-                        <ScrollLink to="testInformation" smooth={true} duration={500}>
-                            <Button color="inherit" style={styles.linkButton}>{t('testInformation')}</Button>
-                        </ScrollLink>
-                        <ScrollLink to="about" smooth={true} duration={500}>
-                            <Button color="inherit" style={styles.linkButton}>{t('about')}</Button>
-                        </ScrollLink>
-                        <ScrollLink to="faq" smooth={true} duration={500}>
-                            <Button color="inherit" style={styles.linkButton}>{t('faq')}</Button>
-                        </ScrollLink>
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <LanguageMenu />
-                        <Button type="button" variant="outlined" onClick={handleLogin} sx={styles.LoginText} >
-                            {t('login')}
-                        </Button>
-                    </Box>
+
+                    {isMobile ? (
+                        <>
+                            <Box sx={{ display: 'flex', alignItems: 'center', marginRight: '8px', maxWidth: '100%' }}>
+                                <LanguageMenu />
+                                <IconButton
+                                    edge="end"
+                                    aria-label="menu"
+                                    onClick={toggleDrawer(true)}
+                                    sx={{ marginLeft: '5px', color: '#185D8E' }}
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                            </Box>
+                            <Drawer
+                                anchor="right"
+                                open={drawerOpen}
+                                onClose={toggleDrawer(false)}
+                                sx={{ '& .MuiDrawer-paper': { backgroundColor: '#D9EEFF', width: '250px', padding: '2rem 1rem'} }}
+                            >
+                                {menuItems}
+                            </Drawer>
+                        </>
+                    ) : (
+                        <>
+                            <Box sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                position: 'absolute',
+                                top: '50%',
+                                left: '50%',
+                                transform: 'translate(-50%, -50%)',
+                            }}>
+                                <ScrollLink to="testInformation" smooth={true} duration={500}>
+                                    <Button color="inherit" sx={linkButton}>{t('testInformation')}</Button>
+                                </ScrollLink>
+                                <ScrollLink to="about" smooth={true} duration={500}>
+                                    <Button color="inherit" sx={linkButton}>{t('about')}</Button>
+                                </ScrollLink>
+                                <ScrollLink to="faq" smooth={true} duration={500}>
+                                    <Button color="inherit" sx={linkButton}>{t('faq')}</Button>
+                                </ScrollLink>
+                            </Box>
+                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                <LanguageMenu />
+                                <Button type="button" variant="outlined" onClick={handleLogin} sx={LoginText}>
+                                    {t('login')}
+                                </Button>
+                            </Box>
+                        </>
+                    )}
                 </Toolbar>
             </Container>
         </AppBar>
