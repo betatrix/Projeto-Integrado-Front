a0c9a931c6fc1b24f22110a332851f72ecef71ea
Beatriz Andrade <b.dandrade@hotmail.com>
2024-10-28 00:49:25 -0300

    feat(courses): add course cards details and responsivity

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,6 @@ import HomePage from '../containers/student/homePage';
 import StudentDashboard from '../containers/student/studentDashboard';
 import InstitutionList from '../containers/student/studentDashboard/searchInstitution';
 import About from '../containers/student/homePage/about';
-import CourseList from '../containers/administrator/courseManagement';
 import PerfilStudent from '../containers/student/studentDashboard/perfilStudent';
 import VocacionalTest from '../containers/student/vocacionalTest';
 import Login from '../containers/auth/login/index';
@@ -22,6 +21,8 @@ import { AuthProvider } from '../contexts/auth';
 import PrivateRoute from '../components/routes/privateRoutes';
 import CadastroCurso from '../containers/administrator/courseRegister';
 import ResultScreen from '../containers/student/resultTest/new-index';
+import CourseManagement from '../containers/administrator/courseManagement';
+import CourseList from '../containers/student/studentDashboard/searchCourse';
 
 export const AppRoutes = () => {
     return (
@@ -37,6 +38,7 @@ export const AppRoutes = () => {
                         <Route path="/success-change-password" element={<SucessPassword />} />
                         <Route path='/sobre' element={<About />} />
                         <Route path='/pagina-inicial' element={<HomePage />} />
+
                         {/* Private Routes - Student */}
                         <Route element={<PrivateRoute requiredRole="ESTUDANTE" />}>
                             <Route path='/teste-vocacional' element={<VocacionalTest />} />
@@ -44,20 +46,21 @@ export const AppRoutes = () => {
                             <Route path="/resultado" element={<ResultScreen />} />
                             <Route path='/minha-conta' element={<PerfilStudent />} />
                             <Route path='/instituicao' element={<InstitutionList />} />
+                            <Route path='/curso' element={<CourseList />} />
                         </Route>
+
                         {/* Private Routes - Admin */}
                         <Route element={<PrivateRoute requiredRole="ADMIN" />}>
                             <Route path="/cadastro" element={<CadastroInstituicao />} />
                             <Route path="/cadastro-curso" element={<CadastroCurso />} />
                             <Route path="/cursos" element={<BuscaCurso />} />
                             <Route path='/politicas' element={<BuscaPoliticas />} />
-                            <Route path='/gerenciamento-curso' element={<CourseList />} />
+                            <Route path='/gerenciamento-curso' element={<CourseManagement />} />
                             <Route path="/gerenciamento-instituicao" element={<InstitutionManagement />} />
                             <Route path='/admin' element={<Dashboard />} />
                         </Route>
 
                         <Route path='*' element={<Navigate to='/pagina-inicial' replace />} />
-                        {/* <Route path="*" element={<Navigate to="/login" replace />} /> */}
 
                     </Routes>
                 </InstitutionProvider>
